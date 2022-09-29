e8e86f662caab0a5fb6987eb1100048567e52400
Hanifa Rifnaz <hanifarifnazz@gmail.com>
2022-09-29 11:38:38 +0530

    Implemented some trophy part

--- eSport/pgTrophy.aspx.cs
+++ eSport/pgTrophy.aspx.cs
@@ -1,5 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,9 +13,16 @@ namespace e_sport_trial.admin
 {
     public partial class pgTrophy : System.Web.UI.Page
     {
+        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ToString());
+
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            // Check the Connection Status & close/Open it
+            if (con.State == ConnectionState.Open)
+            {
+                con.Close();
+            }
+            con.Open();
         }
     }
 }
